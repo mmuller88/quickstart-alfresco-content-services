551a2b0050e0e2a92928c9a0411de4e984a5ee42
Abdul Mohammed <abdul.mohammed@alfresco.com>
2017-08-08 16:52:26 +0100

    Undo comments for scripts

--- scripts/run-share.sh
+++ scripts/run-share.sh
@@ -9,10 +9,10 @@ service solr stop
 chkconfig --del solr
 rm -rf /opt/alfresco-search-services /var/solr /etc/init.d/solr /etc/sysconfig/solr /var/log/solr /etc/default/solr.in.sh
 userdel -r solr
-# rm -fr /etc/chef/chef-client.json
+rm -fr /etc/chef/chef-client.json
 rm -fr /etc/chef/nodes/*.json
 chmod 700 /usr/share/tomcat/shared/classes/alfresco-global.properties
 chmod 700 /usr/share/tomcat/shared/classes/alfresco/web-extension/share-cluster-application-context.xml
-# rm -fr /etc/chef/replaceValues.sh
-# rm -fr /etc/chef/run.sh
+rm -fr /etc/chef/replaceValues.sh
+rm -fr /etc/chef/run.sh
 systemctl restart tomcat-alfresco
